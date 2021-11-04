d963f1ae1eeb422e2fc7d77d9f81ad0287e7542c
djibril-sal <78761473+djibril-sal@users.noreply.github.com>
2021-11-04 14:08:49 +0100

    Add files via upload

--- src/com/sdzee/tp/servlets/CreationCommande.java
+++ src/com/sdzee/tp/servlets/CreationCommande.java
@@ -4,37 +4,56 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.sdzee.tp.beans.Client;
-import com.sdzee.tp.forms.CreationClientForm;
+import com.sdzee.tp.beans.Commande;
+import com.sdzee.tp.forms.CreationCommandeForm;
 
-public class CreationClient extends HttpServlet{
+public class CreationCommande extends HttpServlet {
 	
-	public static final String ATT_CLIENT = "client";
+	public static final String ATT_COMMANDE = "commande";
 	public static final String ATT_FORM = "form";
-	public static final String VUE_SUCCES = "/WEBINF/afficherClient.jsp";
-	public static final String VUE_FORM = "/WEB-INF/creerClient.jsp";
+	public static final String VUE_SUCCES = "/WEBINF/afficherCommande.jsp";
+	public static final String VUE_FORM = "/WEB-INF/creerCommande.jsp";
 	
-	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 	
-		/* À la réception d'une requête GET, simple affichage du
-	formulaire */
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
+	{
+	/* À la réception d'une requête GET, simple affichage du formulaire */
+	
 		this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
 	}
+	
 	
 	public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 	
 		/* Préparation de l'objet formulaire */
-		CreationClientForm form = new CreationClientForm();
+	
+		CreationCommandeForm form = new CreationCommandeForm();
+	
 		/* Traitement de la requête et récupération du bean en résultant */
-		Client client = form.creerClient( request );
+	
+		Commande commande = form.creerCommande( request );
+	
 		/* Ajout du bean et de l'objet métier à l'objet requête */
-		request.setAttribute( ATT_CLIENT, client ); request.setAttribute( ATT_FORM, form );
+	
+		request.setAttribute( ATT_COMMANDE, commande );
+	
+		request.setAttribute( ATT_FORM, form );
+	
 		if ( form.getErreurs().isEmpty() ) {
+	
 			/* Si aucune erreur, alors affichage de la fiche récapitulative */
+	
 			this.getServletContext().getRequestDispatcher( VUE_SUCCES ).forward( request, response );
+	
 		} else {
+	
 			/* Sinon, ré-affichage du formulaire de création avec les erreurs */
-		 this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
+	
+			this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
+
 		}
+
 	}
+	
+	
 }
